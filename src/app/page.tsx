1047738cac4e2a6b18b1dd3282e2849d15439c4c
MatiasOgorinsky <matiogorinsky@gmail.com>
2024-06-26 11:15:13 +0300

    adding loading to game

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,12 +12,14 @@ const Game = () => {
   const [questionNumber, setQuestionNumber] = useState(0);
   const [selectedOption, setSelectedOption] = useState(null);
   const [isCorrect, setIsCorrect] = useState(null);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     fetchData();
   }, []);
 
   const fetchData = async () => {
+    setLoading(true);
     const fetchedPlayers = await fetchPlayers();
     const fetchedRandomNames = await fetchRandomNames();
 
@@ -25,6 +27,7 @@ const Game = () => {
     setRandomNames(fetchedRandomNames);
 
     startGame(fetchedPlayers, fetchedRandomNames);
+    setLoading(false);
   };
 
   const startGame = (fetchedPlayers, fetchedRandomNames) => {
@@ -80,31 +83,37 @@ const Game = () => {
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen p-8">
-      <h1 className="text-3xl font-bold mb-4 text-center">Guess the Football Player</h1>
-      {questionNumber < 10 ? (
-        <>
-          <div className="mb-4">{currentPlayer?.image && <img src={currentPlayer.image} alt={currentPlayer.name} style={{ width: "280px", height: "auto" }} className="rounded-lg shadow-lg mb-4" />}</div>
-          <div className="grid grid-cols-1 gap-4 w-full max-w-md">
-            {options.map((name, index) => (
-              <button
-                key={index}
-                className={`w-90 text-black font-bold py-2 px-4 rounded ${selectedOption ? (name === currentPlayer.name ? "bg-green-500" : name === selectedOption ? "bg-red-500" : "bg-white") : "bg-white hover:bg-orange-400"}`}
-                onClick={() => handleOptionClick(name)}
-                disabled={selectedOption !== null}
-              >
-                {name}
-              </button>
-            ))}
-          </div>
-        </>
+      {loading ? (
+        <h1 className="text-3xl font-bold mb-4 text-center">Loading game...</h1>
       ) : (
-        <div className="text-center">
-          <p className="text-xl font-semibold">Game Over!</p>
-          <p className="text-xl">Your final score is {score}</p>
-          <button className="mt-4 bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded" onClick={handlePlayAgain}>
-            Play Again
-          </button>
-        </div>
+        <>
+          <h1 className="text-3xl font-bold mb-4 text-center">Guess the Football Player</h1>
+          {questionNumber < 10 ? (
+            <>
+              <div className="mb-4">{currentPlayer?.image && <img src={currentPlayer.image} alt={currentPlayer.name} style={{ width: "280px", height: "auto" }} className="rounded-lg shadow-lg mb-4" />}</div>
+              <div className="grid grid-cols-1 gap-4 w-full max-w-md">
+                {options.map((name, index) => (
+                  <button
+                    key={index}
+                    className={`w-90 text-black font-bold py-2 px-4 rounded ${selectedOption ? (name === currentPlayer.name ? "bg-green-500" : name === selectedOption ? "bg-red-500" : "bg-white") : "bg-white hover:bg-orange-400"}`}
+                    onClick={() => handleOptionClick(name)}
+                    disabled={selectedOption !== null}
+                  >
+                    {name}
+                  </button>
+                ))}
+              </div>
+            </>
+          ) : (
+            <div className="text-center">
+              <p className="text-xl font-semibold">Game Over!</p>
+              <p className="text-xl">Your final score is {score}</p>
+              <button className="mt-4 bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded" onClick={handlePlayAgain}>
+                Play Again
+              </button>
+            </div>
+          )}
+        </>
       )}
     </div>
   );
