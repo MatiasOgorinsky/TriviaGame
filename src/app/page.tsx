a52d111d25c226668f0e790d4bf30346a191b185
MatiasOgorinsky <matiogorinsky@gmail.com>
2024-06-27 11:50:48 +0300

    css changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,7 +19,7 @@ const GameComponent = () => {
         <div className="flex flex-col items-center justify-center min-h-screen p-8">
           <input type="text" placeholder="Enter your name" onChange={(e) => setUserName(e.target.value)} className="border-2 border-gray-500 p-2 rounded" />
           <button className="bg-orange-400 mt-10 px-6 py-3 text-lg rounded text-white" onClick={() => handleStartGame(userName)}>
-            Start Game
+            Start Game >
           </button>
         </div>
       ) : (
